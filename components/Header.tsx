d0d22411fbe9cfe1a4d057adc87a0902c448fdf2
Caetano Almeida <caetanoaraujo_@hotmail.com>
2023-07-15 15:50:37 -0300

    fix: Transparency

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,18 +12,23 @@ import {
 	MenuList,
 	MenuItem,
 	Img,
+	Link,
+	useColorModeValue,
 } from '@chakra-ui/react';
-import { TbLanguageHiragana, TbMoonFilled } from 'react-icons/tb';
+import { ReactElement } from 'react';
+import { TbLanguage, TbMoonFilled, TbSunHigh } from 'react-icons/tb';
 
 const Header = () => {
 	const { toggleColorMode } = useColorMode();
+	const color: string = useColorModeValue('#f5f5f5', '#050609');
+	const darkModeIcon: ReactElement = useColorModeValue(<TbMoonFilled />, <TbSunHigh />);
 
 	return (
-		<Flex justifyContent="space-between" p={4} position="sticky" top="0">
-			<Text fontSize="xl" letterSpacing="wide">
-				caealmeida
-			</Text>
-			<Breadcrumb separator="" spacing="30px" fontSize="lg" mt={1}>
+		<Flex justifyContent="space-between" p={3} position="sticky" top={0} left={0} right={0} zIndex="999" bg={color}>
+			<Link href="#" fontSize="xl" letterSpacing="wide">
+				<Text as="b">Caetano Almeida</Text>
+			</Link>
+			<Breadcrumb separator="" spacing="30px" fontSize="lg">
 				<BreadcrumbItem>
 					<BreadcrumbLink href="#">Sobre</BreadcrumbLink>
 				</BreadcrumbItem>
@@ -37,7 +42,7 @@ const Header = () => {
 			<Flex>
 				<Menu>
 					<Tooltip label="Idioma" closeDelay={250} fontSize="sm">
-						<MenuButton as={IconButton} aria-label="Languages" icon={<TbLanguageHiragana />} variant="ghost" />
+						<MenuButton as={IconButton} aria-label="Languages" icon={<TbLanguage />} variant="ghost" />
 					</Tooltip>
 					<MenuList>
 						<MenuItem>
@@ -49,7 +54,7 @@ const Header = () => {
 					</MenuList>
 				</Menu>
 				<Tooltip label="Dark Mode" closeDelay={250} fontSize="sm">
-					<IconButton aria-label="dark-mode" variant="ghost" icon={<TbMoonFilled />} onClick={toggleColorMode} />
+					<IconButton aria-label="dark-mode" variant="ghost" icon={darkModeIcon} onClick={toggleColorMode} />
 				</Tooltip>
 			</Flex>
 		</Flex>
@@ -57,5 +62,3 @@ const Header = () => {
 };
 
 export default Header;
-
-// <a href="https://www.flaticon.com/free-icons/brazil" title="brazil icons">Brazil icons created by Freepik - Flaticon</a>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,23 @@
-import { Box, Button, Flex, Heading, useColorModeValue } from '@chakra-ui/react';
+import { Box, Button, Flex, Heading, Divider, useColorModeValue, Container } from '@chakra-ui/react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 const IndexPage = () => {
 	const color = useColorModeValue('blackAlpha.50', 'blackAlpha.800');
+
 	return (
-		<Box backgroundColor={color}>
-			<Header />
-			<Flex height="100vh" alignItems="center" justifyContent="center">
-				<Flex direction="column" p={12} rounded={6}>
-					<Heading>Hello Next.js 👋</Heading>
-					<Button colorScheme="red">TESTE</Button>
+		<Box background={color}>
+			<Container maxW="95%" margin="auto">
+				<Header />
+				<Flex height="100vh" alignItems="center" justifyContent="center">
+					<Flex direction="column" p={12} pb={900}>
+						<Heading>Hello Next.js 👋</Heading>
+						<Button colorScheme="red">TESTE</Button>
+					</Flex>
 				</Flex>
-			</Flex>
+				<Divider />
+				<Footer />
+			</Container>
 		</Box>
 	);
 };
