3b426c23f224d5fbbe3a9b20157067fdd823aef1
Caetano Almeida <caetanoaraujo_@hotmail.com>
2023-07-15 17:12:47 -0300

    feat: add colors

--- utils/Theme.ts
+++ utils/Theme.ts
@@ -3,8 +3,12 @@ import { extendTheme } from '@chakra-ui/react'
 const theme = extendTheme({
   fonts: {
     body: `'Inter', sans-serif`,
-    heading: `'Inter', sans-serif`,
+    heading: `'Inter', sans-serif`
   },
+  colors: {
+    light: '#f5f5f5',
+    dark: '#050609'
+  }
 })
 
 export default theme
