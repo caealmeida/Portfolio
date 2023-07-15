de26e680f4dccfe7a06fd2a4dc32a44b4c48200a
Caetano Almeida <caetanoaraujo_@hotmail.com>
2023-07-15 15:50:14 -0300

    feat: add footer

---
+++ components/Footer.tsx
@@ -0,0 +1,28 @@
+import { Flex, Icon, Link, Spacer, Text } from '@chakra-ui/react';
+import { TbBrandLinkedin, TbBrandGithub, TbMail } from 'react-icons/tb';
+
+const Footer = () => {
+	const year = new Date().getFullYear();
+
+	return (
+		<Flex justifyContent="center" p={4}>
+			<Text fontSize="sm" letterSpacing="wide">
+				caealmeida © {year}
+			</Text>
+			<Spacer />
+			<Flex justifyContent="space-between">
+				<Link href="https://www.linkedin.com/in/caealmeida/" isExternal>
+					<Icon as={TbBrandLinkedin} boxSize={8} />
+				</Link>
+				<Link href="https://github.com/caealmeida" mx={5} isExternal>
+					<Icon as={TbBrandGithub} boxSize={8} />
+				</Link>
+				<Link href="mailto:cae.almeida1@gmail.com" isExternal>
+					<Icon as={TbMail} boxSize={8} />
+				</Link>
+			</Flex>
+		</Flex>
+	);
+};
+
+export default Footer;
