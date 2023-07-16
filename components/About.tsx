ce778fd9834e500bc2996c08cf9ac0052b28a297
Caetano Almeida <caetanoaraujo_@hotmail.com>
2023-07-16 14:45:09 -0300

    feat: add about

---
+++ components/About.tsx
@@ -0,0 +1,9 @@
+import { Flex, Text } from '@chakra-ui/react';
+
+const About = () => (
+	<Flex id="about" p={2} mt={600} border="1px" borderColor="red">
+		<Text fontSize="2.5em">Sobre</Text>
+	</Flex>
+);
+
+export default About;
