69b7afb665e3154a6aca56522882d666bf974cc6
caealmeida <caetanoaraujo_@hotmail.com>
2023-07-17 11:27:01 -0300

    style: add head name

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,22 +5,28 @@ import Home from '../components/Home';
 import About from '../components/About';
 import Projects from '../components/Projects';
 import Contacts from '../components/Contacts';
+import Head from 'next/head';
 
 const IndexPage = () => {
   const color = useColorModeValue('blackAlpha.100', 'blackAlpha.800');
 
   return (
-    <Box background={color}>
-      <Container maxW='95%' margin='auto'>
-        <Header />
-        <Home />
-        <About />
-        <Contacts />
-        <Projects />
-        <Divider />
-        <Footer />
-      </Container>
-    </Box>
+    <>
+      <Head>
+        <title>caealmeida</title>
+      </Head>
+      <Box background={color}>
+        <Container maxW='95%' margin='auto'>
+          <Header />
+          <Home />
+          <About />
+          <Contacts />
+          <Projects />
+          <Divider />
+          <Footer />
+        </Container>
+      </Box>
+    </>
   );
 };
 
