11c62c8c06b2e4031905a59755a02a0776334f9a
Caetano Almeida <caetanoaraujo_@hotmail.com>
2023-07-16 21:45:05 -0300

    style: width adjust

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,23 +1,23 @@
 import { Flex, Heading, Text } from '@chakra-ui/react';
 
 const Home = () => (
-  <Flex p={2} justifyContent='center' mt={20}>
-    <Flex direction='column' maxW='75%'>
-      <Text fontSize='5em' p={6}>
-        Fullstack
-      </Text>
-      <Heading as='h1' size='4xl' fontSize='10em'>
-        Software Developer
-      </Heading>
-    </Flex>
-    <Flex direction='column' justifyContent='flex-end' mb={5}>
-      <Text fontSize='1.8em'>Java</Text>
-      <Text fontSize='1.8em'>JavaScript</Text>
-      <Text fontSize='1.8em'>TypeScript</Text>
-      <Text fontSize='1.8em'>Python</Text>
-      <Text fontSize='1.8em'>C#</Text>
-    </Flex>
-  </Flex>
+	<Flex p={2} justifyContent="center" mt={20}>
+		<Flex direction="column" maxW="75%">
+			<Text fontSize="5em" p={2}>
+				Fullstack
+			</Text>
+			<Heading as="h1" size="4xl" fontSize="10em">
+				Software Developer
+			</Heading>
+		</Flex>
+		<Flex direction="column" justifyContent="flex-end" mb={5}>
+			<Text fontSize="1.8em">Java</Text>
+			<Text fontSize="1.8em">JavaScript</Text>
+			<Text fontSize="1.8em">TypeScript</Text>
+			<Text fontSize="1.8em">Python</Text>
+			<Text fontSize="1.8em">C#</Text>
+		</Flex>
+	</Flex>
 );
 
 export default Home;
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -2,20 +2,20 @@ import { Flex, Heading, Divider } from '@chakra-ui/react';
 import Project from './Project';
 
 const Projects = () => {
-  return (
-    <Flex id='projects' direction='column' justifyContent='center' mt={'32'}>
-      <Heading as='h3' size='xl' py={2}>
-        Projetos:
-      </Heading>
-      <Divider />
-      <Flex direction='column'>
-        <Project name={'Portfolio'} link={'https://github.com/caealmeida/Portfolio'} />
-        <Project name={'E-Commerce'} link={'https://github.com/caealmeida/shopping-cart'} />
-        {/* <Project />
+	return (
+		<Flex id="projects" direction="column" justifyContent="center" maxW="85%" m="auto" mt={'32'} mb={'32'}>
+			<Heading as="h3" size="xl" py={2}>
+				Projetos:
+			</Heading>
+			<Divider />
+			<Flex direction="column">
+				<Project name={'Portfolio'} link={'https://github.com/caealmeida/Portfolio'} />
+				<Project name={'E-Commerce'} link={'https://github.com/caealmeida/shopping-cart'} />
+				{/* <Project />
       <Project /> */}
-      </Flex>
-    </Flex>
-  );
+			</Flex>
+		</Flex>
+	);
 };
 
 export default Projects;
