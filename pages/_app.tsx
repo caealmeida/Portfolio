8409ad8b69cc92a63b77fbbae114274d42375fd3
caealmeida <caetanoaraujo_@hotmail.com>
2023-07-17 12:47:52 -0300

    chore: add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { Analytics } from '@vercel/analytics/react';
 import theme from '../public/Theme';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
+      <Analytics />
     </ChakraProvider>
   );
 }
